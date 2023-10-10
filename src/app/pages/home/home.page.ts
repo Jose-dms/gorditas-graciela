462d1e8e3f0b1e604d649f19b19cd7a3a68a6e7b
jose_dms <019001412@upsrj.edu.mx>
2023-10-09 22:38:10 -0600

    agrefue la barra de segmentos

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+selectTabs = 'Pedidos nuevos';
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+
+
+}
